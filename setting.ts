23fc63d4a47c8dd07179da07ea32321993cdb26a
名前 <>
2026-09-20 12:51:06 +0000

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
-// ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+// ex) export const TARGET_URL = "https://yutakakato2026.github.io/tech-blog.github.io/top.html";
+export const TARGET_URL = "https://yutakakato2026.github.io/tech-blog.github.io/";
